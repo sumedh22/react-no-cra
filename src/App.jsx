bd37e3e3570a4760892bba4f6c864ee89436d0d7
Sumedh Chakravorty <sumedh@Sumedhs-MacBook-Pro.local>
2020-05-23 15:37:35 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default () => {
+    return <div>
+        Hello from React app!
+    </div>
+}
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: `${__dirname}/build`,
+        filename: '[name].[hash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: "babel-loader",
+                    query: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'css-loader',
+                    'style-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        })
+    ],
+    resolve:{
+        extensions:['.js','.jsx','.css']
+    }
+}
